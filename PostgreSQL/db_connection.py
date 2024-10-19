acba9b2253cc96e03baf96ff2b10a83f628970c0
shakuntala <sakunsah61@gmail.com>
2024-10-19 12:51:13 +0530

    hello

--- PostgreSQL/db_connection.py
+++ PostgreSQL/db_connection.py
@@ -8,10 +8,19 @@ import os
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
+    try:
+        
+        return psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+    except:
+        print("connection did not created",ex)
+con = get_connection()
+if con:
+    print("connection establish sucessfully.")
+else:
+    print("connection did not establish.")       
